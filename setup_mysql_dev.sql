d84a585e63f2c5637ef8f6f7ba173470e4713e79
chinomso okoye <chinomsokoye@yahoo.com>
2022-09-13 15:55:35 -0700

    Prepare MySQL server for project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
 -- Prepares a MySQL server for the project
--- Creates a database
--- Grant user priviledges on creation
--- Grant SELECT priviledges on the performance schema
+-- Grant user on *.* TO hbnb_dev@localhost
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-GRANT ALL ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost'
-IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
+USE hbnb_dev_db;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+USE performance_schema;
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
